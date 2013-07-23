37bbc8359859113e65bcaca77fc09a531f3021c2
mahender20 <mahenderp@gmail.com>
2013-07-23 19:33:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+var express = require('express'),
+    app = express.createServer(express.logger()),
+    fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World from index.html');
-//fs.readfile('index.html');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(fs.readFileSync("./index.html", "utf8").toString());
+    response.end();
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
+
+
